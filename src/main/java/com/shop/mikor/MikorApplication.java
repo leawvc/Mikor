656c60c22972dcf6a78ec0663f7a4350053f3d1d
eoeo777755@naver.com <eoeo777755@naver.com>
2023-12-01 15:24:43 +0900

    Initial commit

---
+++ src/main/java/com/shop/mikor/MikorApplication.java
@@ -0,0 +1,13 @@
+package com.shop.mikor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/mikor/MikorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.mikor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
