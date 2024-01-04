1df7d925b1346c997fd4fb7d57dfcab9032a0553
eoeo777755@naver.com <eoeo777755@naver.com>
2024-01-04 21:19:17 +0900

    TEST CODE

---
+++ src/main/java/com/shop/mikor/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.shop.mikor.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/main/java/com/shop/mikor/web/PostsApiController.java
@@ -0,0 +1,28 @@
+package com.shop.mikor.web;
+
+import com.shop.mikor.service.PostsService;
+import com.shop.mikor.web.dto.PostsResponseDto;
+import com.shop.mikor.web.dto.PostsSaveRequestDto;
+import com.shop.mikor.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+    @PutMapping("/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+    @GetMapping("/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
